4c82e27e1a4a7719a82d46952367ca3c4547e94b
Atharva Aalok <atharvaaalok@gmail.com>
2025-11-18 14:09:03 -0800

    Add training scripts for NIGnet and NICE.

---
+++ run_train_nice.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+LOG_NAME="log1_nice_small"
+
+for SEED in {0..9}; do
+    echo "Running with SEED=${SEED}"
+
+    RUN_DIR="logs/${LOG_NAME}/seed_${SEED}"
+
+    # Create directory
+    mkdir -p "$RUN_DIR"
+
+    # Run training and save output to a log file in that directory
+    uv run train_nice.py --seed "$SEED" --log-name "$LOG_NAME" > "${RUN_DIR}/main_log.txt"
+done
